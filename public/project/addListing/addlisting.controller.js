a76c04d384ba601467a92ba195a57bd03091d1dc
Sanjay Rajashekhar <91sanjay@gmail.com>
2015-11-05 20:07:42 -0500

    Add gallery for search result
    1. Add gallery
    2. Embed example map
    3. Add image for initial search result

--- public/project/addListing/addlisting.controller.js
+++ public/project/addListing/addlisting.controller.js
@@ -1,3 +1,11 @@
-/**
- * Created by sanjay on 10/31/15.
- */
+"use strict";
+
+(function() {
+    angular
+        .module("RentEasy")
+        .controller("AddListingController", AddListingController);
+
+    function AddListingController($scope, $location) {
+        $scope.$location = $location;
+    }
+})();
