c9c906da099d13c36c8a42c9e8f272bd7f83a4e8
Sanjay Rajashekhar <91sanjay@gmail.com>
2015-11-21 21:04:35 -0500

    Field Controller

---
+++ public/assignment/client/views/field/field.controller.js
@@ -0,0 +1,110 @@
+"use strict";
+
+(function() {
+    angular
+        .module("FormBuilderApp")
+        .controller("FieldController", FieldController);
+
+    function FieldController($scope, $location, $rootScope, FieldService) {
+        $scope.location = $location;
+        $scope.user = $rootScope.currentUser;
+        $scope.fields = [];
+        $scope.fieldType = null;
+        $scope.addField = addField;
+        $scope.removeField = removeField;
+
+        $rootScope.$on("login", function(event, user){
+            $scope.user = $rootScope.currentUser = user;
+        });
+
+        $rootScope.$on("selectedForm", function(event, form){
+            $scope.selectedForm = $rootScope.selectedForm = form;
+        });
+
+        function initFields() {
+            FieldService.getFormFields($scope.selectedForm.id).then(function(fields) {
+                $scope.fields = fields;
+            });
+        }
+
+        initFields();
+
+        function addField(fieldType) {
+            var field = getField(fieldType);
+            //$scope.fields.push(field);
+
+            FieldService.createFieldForForm($scope.selectedForm.id, field).then(function(fields) {
+                $scope.fields = fields;
+            });
+        }
+
+        function getField(fieldType) {
+            var field;
+            switch(fieldType) {
+                case "TEXT":
+                    field = {
+                        "id": null,
+                        "label": "New Text Field",
+                        "type": "TEXT",
+                        "placeholder": "New Field"
+                    };
+                    break;
+                case "TEXTAREA":
+                    field = {
+                        "id": null,
+                        "label": "New Text Field",
+                        "type": "TEXTAREA",
+                        "placeholder": "New Field"
+                    };
+                    break;
+                case "DATE":
+                    field = {
+                        "id": null,
+                        "label": "New Date Field",
+                        "type": "DATE"
+                    };
+                    break;
+                case "OPTIONS":
+                    field = {
+                        "id": null,
+                        "label": "New Dropdown",
+                        "type": "OPTIONS",
+                        "options": [{"label": "Option 1", "value": "OPTION_1"}, {"label": "Option 2", "value": "OPTION_2"}, {"label": "Option 3", "value": "OPTION_3"}]
+                    };
+                    break;
+                case "EMAIL":
+                    field = {
+                        "id": null,
+                        "label": "New Email Field",
+                        "type": "EMAIL",
+                        "placeholder": "New Email Field"
+                    };
+                    break;
+                case "CHECKBOXES":
+                    field = {
+                        "id": null,
+                        "label": "New Checkboxes",
+                        "type": "CHECKBOXES",
+                        "options": [{"label": "Option 1", "value": "OPTION_1"}, {"label": "Option 2", "value": "OPTION_2"}, {"label": "Option 3", "value": "OPTION_3"}]
+                    };
+                    break;
+                case "RADIOS":
+                    field = {
+                        "id": null,
+                        "label": "New Radio Buttons",
+                        "type": "RADIOS",
+                        "options": [{"label": "Option 1", "value": "OPTION_1"}, {"label": "Option 2", "value": "OPTION_2"}, {"label": "Option 3", "value": "OPTION_3"}]
+                    };
+                    break;
+            }
+
+            return field;
+        }
+
+        function removeField(field) {
+            FieldService.deleteFieldFromForm($scope.selectedForm.id, field.id).then(function(fields) {
+                $scope.fields = fields;
+            })
+        }
+    }
+})();
