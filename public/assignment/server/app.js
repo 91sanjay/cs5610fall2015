d6a903040b9ea1ff685e716ef01eb52b47232cf3
Sanjay Rajashekhar <91sanjay@gmail.com>
2015-11-27 00:55:52 -0500

    server side app.js

--- public/assignment/server/app.js
+++ public/assignment/server/app.js
@@ -1,8 +1,8 @@
 "use strict";
 
-module.exports = function(app) {
-    var user =  require("./models/user.model.js")(app);
-    var form =  require("./models/form.model.js")(app);
+module.exports = function(app, mongoose, db) {
+    var user =  require("./models/user.model.js")(app, mongoose, db);
+    var form =  require("./models/form.model.js")(app, mongoose, db);
 
     require("./services/user.service.js")(app, user);
     require("./services/form.service.js")(app, form);
