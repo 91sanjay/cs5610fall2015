4adbe66f876e137abe581b27d2463d92e7e798a6
Sanjay Rajashekhar <91sanjay@gmail.com>
2015-11-27 00:44:49 -0500

    Start Assignment 5 - Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var mongoose = require('mongoose');
+var connectionString = 'mongodb://127.0.0.1:27017/cs5610';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+var db = mongoose.connect(connectionString);
+require("./public/assignment/server/app.js")(app, mongoose, db);
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-require("./public/assignment/server/app.js")(app);
 
 app.listen(port, ipaddress);
