787cc01d83e67eff22cee35019081ce55718b657
Sanjay Rajashekhar <91sanjay@gmail.com>
2015-11-27 01:02:03 -0500

    User Schema

--- public/assignment/server/models/user.schema.js
+++ public/assignment/server/models/user.schema.js
@@ -1,2 +1,15 @@
 "use strict"
 
+module.exports = function(mongoose) {
+    var UserSchema = new mongoose.Schema({
+        "firstName": String,
+        "lastName": String,
+        "username": String,
+        "password": String,
+        "email": String
+    });
+
+    this.getSchema = function() {
+        return UserSchema;
+    }
+}
