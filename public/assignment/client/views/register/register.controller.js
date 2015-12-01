95dafefd084a0acc1fadd1f23822b8eeb7c3902d
Sanjay Rajashekhar <91sanjay@gmail.com>
2015-12-01 01:25:17 -0500

    Improve register checks and display error alerts

--- public/assignment/client/views/register/register.controller.js
+++ public/assignment/client/views/register/register.controller.js
@@ -10,33 +10,56 @@
         $scope.register = register;
 
         function register() {
+            var exits = false;
+            $scope.regError = null;
 
-        }
+            if ($scope.userName && $scope.password && $scope.verifyPassword && $scope.email) {
+                if ($scope.password != $scope.verifyPassword) {
+                    $scope.regError = "Passwords do not match";
+                } else {
+                    UserService.findAllUsers()
+                        .then(function (users) {
+                            console.log(users);
+                            for (var i = 0; i < users.length; i++) {
+                                if (users[i].username === $scope.userName) {
+                                    var message = "User Name already exists. Please choose a different one. We accept only letters and numbers in usernames";
 
-        function rregister() {
-            var userName = $scope.userName;
-            var password = $scope.password;
-            var email = $scope.email;
-            if (userName && password && email) {
-                var user = {
-                    "userName": $scope.userName,
-                    "password": $scope.password,
-                    "email": $scope.email
-                };
+                                    if ($scope.regError) {
+                                        $scope.regError += " | " + message;
+                                    } else {
+                                        $scope.regError = message;
+                                    }
+                                }
+                                if (users[i].email === $scope.email) {
+                                    var message = "Email already in use"
 
-                console.log("Registering user " + user.userName);
+                                    if ($scope.regError) {
+                                        $scope.regError += " | " + message;
+                                    } else {
+                                        $scope.regError = message;
+                                    }
+                                }
+                            }
 
-                UserService.createUser(user, function(user) {
-                    if (user.id != null) {
-                        console.log("Not Null");
-                        $location.url("/profile");
-                    } else {
-                        console.log("User already registered. Login to continue");
-                        $location.path("/register");
-                    }
-                });
+                            if (!$scope.regError) {
+                                console.log("enter "+$scope.regError);
+                                var user = {
+                                    username: $scope.userName,
+                                    password: $scope.password,
+                                    email: $scope.email
+                                };
 
-                $rootScope.currentUser = user;
+                                UserService.createUser(user)
+                                    .then(function(newUser){
+                                        $rootScope.currentUser = newUser;
+                                        $rootScope.$broadcast('login', newUser);
+                                        $location.url('/profile');
+                                    });
+                            }
+                        });
+                }
+            } else {
+                $scope.regError = "All fields are required"
             }
         }
     }
