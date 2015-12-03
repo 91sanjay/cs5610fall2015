6cb8c8aecbdbcfaf1a5cda676a195b9e46ee6cb9
Sanjay Rajashekhar <91sanjay@gmail.com>
2015-12-03 12:38:15 -0500

    Use ng model in search page

--- public/project/client/views/search/search.controller.js
+++ public/project/client/views/search/search.controller.js
@@ -1,11 +1,17 @@
 "use strict";
 
-(function() {
+(function () {
     angular
         .module("RentEasy")
         .controller("SearchController", SearchController);
 
     function SearchController($scope, $location) {
         $scope.$location = $location;
+        $scope.fullAddress = "440 Huntington Ave, Boston, MA-02115";
+        $scope.description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Deleniti dolor dolores doloribus eius eligendi enim id inventoreiusto mollitia odit officiis possimus quibusdam quodreprehenderit tempora temporibus vel velit, veniam?";
+        $scope.bed = 2;
+        $scope.bath = 1;
+        $scope.pets = false;
+        $scope.heat = false;
     }
 })();
