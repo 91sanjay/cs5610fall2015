3d2aee80b7e38af800a83bae58625d88ecd1a48d
Sanjay Rajashekhar <91sanjay@gmail.com>
2015-12-18 07:33:30 -0500

    register fix

--- public/project/client/views/profile/profile.controller.js
+++ public/project/client/views/profile/profile.controller.js
@@ -9,9 +9,12 @@
         $scope.$location = $location;
         $scope.update = update;
         $scope.updated = false;
+        var doUpdate = true;
+        $scope.noUpdate = false;
+        $scope.passworderror = false;
         $scope.user = $rootScope.currentUser;
 
-        function update() {
+        function update(user) {
             UserService.Update($scope.user._id, $scope.user).then(function (updatedUser) {
                 $rootScope.currentUser = updatedUser;
                 $scope.updated = true;
